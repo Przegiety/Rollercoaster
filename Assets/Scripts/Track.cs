5f8b620a8902501395b4c4312e0fb8f7e32b327d
Przegiety <t.przegietka@gmail.com>
2017-02-08 21:54:11 +0100

    Praparing for correct shape making with fixed distances.

--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -8,15 +8,22 @@ using Bezier;
 namespace Rollercoaster {
     [ExecuteInEditMode]
     public class Track : MonoBehaviour {
+        [Header("Generation parameters")]
+        [SerializeField]
+        [Range(0,100)]
+        private int _stepsPerCurve = 10;
+        [SerializeField]
+        private float _segmentLength = 1f;
+        [Space]
         [SerializeField]
         private TrackPoint[] _points;
-
+        
+        //For debug purposes only
         private void Update() {
             DebugDraw(10);
         }
 
         private void DebugDraw(int stepCount) {
-
             float step = 1f/stepCount;
 
             var prev = Vector3.zero;
@@ -36,9 +43,30 @@ namespace Rollercoaster {
             }
         }
 
+        private void Make() {
+            //1st step: regular bezier
+            float step = 1f/_stepsPerCurve;
+            var bezierPoints = new Vector3[_stepsPerCurve * _points.Length];
+            for(int j = 0; j < _points.Length; j++) {
+                for(int i = 0; i <= _stepsPerCurve; i++) {
+                    bezierPoints[i + j * _stepsPerCurve] = BezierCurve.Lerp(i * step, _points[j].points.Select(t => t.position).ToArray());
+                }
+            }
+            //2nd step: make points in constant distance
+            var regularPoints = new List<Vector3>();
+            //TODO
+            throw new NotImplementedException();
+        }
+
         [Serializable]
         private struct TrackPoint {
             public Transform[] points;
+        }
+
+        [Serializable]
+        public struct TrackVert {
+            public Vector3 position;
+            public Vector3 normal;
         }
     }
 }
