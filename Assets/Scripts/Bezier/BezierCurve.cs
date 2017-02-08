6f28449ca4ec915126b8f20820f626897927d0ff
Przegiety <t.przegietka@gmail.com>
2017-02-08 20:20:00 +0100

    Initial commit.

---
+++ Assets/Scripts/Bezier/BezierCurve.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bezier {
+    public class BezierCurve {
+        public static Vector3 Lerp(float t, params Vector3[] args) {
+            if(args.Length == 0)
+                throw new ArgumentException();
+            if(args.Length == 1)
+                return args[0];
+            Vector3[] newArgs = new Vector3[args.Length-1];
+            for(int i = 0; i < newArgs.Length; i++) {
+                newArgs[i] = Vector3.Lerp(args[i], args[i + 1], t);
+            }
+            return Lerp(t, newArgs);
+        }
+    }
+}
---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Bezier;
+
+namespace Rollercoaster {
+    [ExecuteInEditMode]
+    public class Track : MonoBehaviour {
+        [SerializeField]
+        private TrackPoint[] _points;
+
+        private void Update() {
+            DebugDraw(10);
+        }
+
+        private void DebugDraw(int stepCount) {
+
+            float step = 1f/stepCount;
+
+            var prev = Vector3.zero;
+            bool _isPrev = false;
+            foreach(var point in _points) {
+                for(int i = 0; i <= stepCount; i++) {
+                    var position = BezierCurve.Lerp(i * step,point.points.Select(t => t.position).ToArray());
+                    if(_isPrev) {
+                        Debug.DrawLine(prev, position);
+                    }
+                    prev = position;
+                    _isPrev = true;
+                }
+                for(int i = 1; i < point.points.Length; i++) {
+                    Debug.DrawLine(point.points[i].position, point.points[i - 1].position, Color.red);
+                }
+            }
+        }
+
+        [Serializable]
+        private struct TrackPoint {
+            public Transform[] points;
+        }
+    }
+}
